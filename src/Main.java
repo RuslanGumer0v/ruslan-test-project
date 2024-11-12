31faf8a65d9280e7078c7aeb60d53c2ae059e38f
RuslanGumer0v <ky3bmu4bbb@gmail.com>
2024-11-12 17:22:04 +0300

    regex

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+
+
+
 public class Main {
     public static void main(String[] args) {
+
+        String a = "1";
+        System.out.println(a.matches("(-|\\+)?\\d{0,2}"));
+
         System.out.println("Hello world!");
     }
 }
